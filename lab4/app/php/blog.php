323d09039cd2ae7d497e21ffc5457794f5f928ff
narongchai11500@gmail.com <narongchai11500@gmail.com>
2025-01-02 11:20:04 +0700

    lab4

--- lab4/app/php/blog.php
+++ lab4/app/php/blog.php
@@ -7,6 +7,36 @@ $method = $_SERVER['REQUEST_METHOD'];
 $response = ['status' => 'error', 'message' => 'Invalid request'];
 
 switch ($method) {
+    case 'GET':
+        if(isset($_GET['id'])) {
+            $id = $_GET['id'];
+            $stmt = $conn->prepare("SELECT * FROM tb_blog WHERE id = ?");
+            $stmt->bind_param("i",$id);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $blog = $result->fetch_assoc();
+            
+            if($blog) {
+                $response = ['status' => 'success','data' => $blog];    
+            } else {
+                $response = ['status' => 'error','message' => 'Blog not found'];
+            }
+        }else{
+            //get all
+            $blog = [];
+            $stmt = $conn->prepare("SELECT * FROM tb_blog");
+            $stmt->execute();
+
+            while($result = $stmt->get_result()) {
+                $blog[] = $result->fetch_assoc();
+            }
+
+            $response = ['status' => 'success','data' => $blog];
+        }
+        break;
+    
+    
+    
     case 'POST': // Insert new blog
         $title = $_POST['title'] ?? null;
         $post = $_POST['post'] ?? null;
